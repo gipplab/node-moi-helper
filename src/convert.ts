5caf998fd767716097ab5230503a97bd5ec6259d
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2019-02-28 19:33:18 +0100

    Ignore documents without text

--- src/convert.ts
+++ src/convert.ts
@@ -1,7 +1,8 @@
 import parse = require('csv-parse');
 import stringify = require('csv-stringify');
 import fs = require('fs');
-let preq:any = require('preq');
+
+let preq: any = require('preq');
 
 interface Record {
   id: number;
@@ -12,51 +13,48 @@ interface Record {
 }
 
 
-export const Convert = (inFile: string, outFile?:string) => {
+export const Convert = (inFile: string, outFile?: string) => {
   const inStream = fs.createReadStream(inFile);
-  const stringifier = stringify({header: true});
+  const stringifier = stringify({ header: true });
   const parser = parse({ columns: true, cast: true });
   const output: Record[] = [];
   let recordSize = 0;
-  if (outFile){
-    const outStream =  fs.createWriteStream(outFile);
+  if (outFile) {
+    const outStream = fs.createWriteStream(outFile);
     stringifier.pipe(outStream);
   }
   parser.on('readable', () => {
     let record;
     while (record = parser.read()) {
-        output.push(record);
-        console.log('Read records ' + ++recordSize);
+      output.push(record);
+      console.log('Read records ' + ++recordSize);
     }
   });
   const one = new Promise<Record[]>((resolve, reject) => {
     parser.on('end', () => {
-      const renderings:Promise<Record>[] =[];
-      output.forEach(x =>{
+      const renderings: Promise<Record>[] = [];
+      output.forEach(x => {
         renderings.push(
-        preq.post({
-            uri: 'https://latexml.mathweb.org/convert',
-            body:{
-              profile: 'fragment',
-              tex: x.text,
+          preq.post({
+              uri: 'https://latexml.mathweb.org/convert',
+              body: {
+                profile: 'fragment',
+                tex: x.text,
+              },
             },
-          },
-        ).then((res:{  body: {log:string, result:string};   }) => {
-          x.mml=res.body.result;
-          stringifier.write(x);
-        }))
+          ).then((res: { body: { log: string, result: string }; }) => {
+            x.mml = res.body.result;
+            stringifier.write(x);
+          }));
       });
       Promise.all(renderings).then(
         () => {
-          if(outFile){
+          if (outFile) {
             stringifier.end();
           }
-          resolve(output)
-        }
+          resolve(output);
+        },
       );
-
-
-
     });
 
   });
--- src/filter.ts
+++ src/filter.ts
@@ -15,42 +15,46 @@ function getIds(ymlIds: string) {
   const doc = yaml.safeLoad(fs.readFileSync(ymlIds, 'utf8'));
   let ids: number[] = [];
   Object.keys(doc).map(key => {
-    ids = Array.from(new Set([...ids, ...(doc[key]||[])]));
+    ids = Array.from(new Set([...ids, ...(doc[key] || [])]));
   });
-  console.log("Filter size " + ids.length);
+  console.log('Filter size ' + ids.length);
   return ids;
 }
 
-function writeLog(r:Record){
+function writeLog(r: Record) {
 
 }
 
-export const Filter = (ymlIds: string, inFile: string, outFile?:string) => {
+export const Filter = (ymlIds: string, inFile: string, outFile?: string) => {
   const inStream = fs.createReadStream(inFile);
-  const stringifier = stringify({header: true});
+  const stringifier = stringify({ header: true });
   const parser = parse({ columns: true, cast: true });
   const ids = getIds(ymlIds);
   const output: Record[] = [];
   let recordSize = 0;
-  if (outFile){
-    const outStream =  fs.createWriteStream(outFile);
+  if (outFile) {
+    const outStream = fs.createWriteStream(outFile);
     stringifier.pipe(outStream);
   }
   parser.on('readable', () => {
     let record;
     while (record = parser.read()) {
       if (ids.includes(record.id)) {
-        output.push(record);
-        console.log('Filtered records ' + ++recordSize);
-        if(outFile){
-          stringifier.write(record);
+        if (record.text.length == 0) {
+          console.log('Skip record without text');
+        } else {
+          output.push(record);
+          console.log('Filtered records ' + ++recordSize);
+          if (outFile) {
+            stringifier.write(record);
+          }
         }
       }
     }
   });
   const one = new Promise<Record[]>((resolve, reject) => {
     parser.on('end', () => {
-      if(outFile){
+      if (outFile) {
         stringifier.end();
       }
       resolve(output);
