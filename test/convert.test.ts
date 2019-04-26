c478125cfb7adae694356a1442c9816c3c4c25d8
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2019-04-26 13:30:44 +0200

    Extend timout for server requests

--- test/convert.test.ts
+++ test/convert.test.ts
@@ -2,7 +2,8 @@ import { Convert } from '../src/convert';
 import { expect } from 'chai';
 import 'mocha';
 
-describe('Convert', () => {
+describe('Convert', function() {
+  this.timeout(20000);
   it('converts',
     () => Convert(__dirname + '/data/export.csv').then(data => expect(data.length).eq(3)),
   );
@@ -10,5 +11,4 @@ describe('Convert', () => {
     () => Convert(__dirname + '/data/export.csv', '/tmp/converted.csv')
       .then(data => expect(data.length).eq(3)),
   );
-});
-
+})
