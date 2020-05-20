5c93146a7c8e7600354657a52fc3cb0bacf0dc97
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-05-20 22:19:03 +0200

    Use real iterator for input

--- src/arq2hrvst.ts
+++ src/arq2hrvst.ts
@@ -22,7 +22,7 @@ const minimize = (mml: any) =>
     .toMinimalPmml('all').toString();
 
 
-function writeFile(outPath: string, data: string, options:{} = { encoding: 'utf8', flag: 'a' }) {
+function writeFile(outPath: string, data: string, options: {} = { encoding: 'utf8', flag: 'a' }) {
   // @ts-ignore
   return afs.writeFileAsync(outPath, data, options).then(() => vlog('wrote to file'));
 }
@@ -42,13 +42,27 @@ export function extract(mml: string, url: string, postId: number) {
 const processFile = (inFile: string, outFile: string) => {
   const inStream = fs.createReadStream(inFile);
   const parser = parse({ columns: true, cast: true, delimiter: '\t' });
+  const iterable = {
+    [Symbol.iterator]() {
+      // noinspection JSUnusedGlobalSymbols
+      return {
+        next() {
+          const record = parser.read();
+          if (record){
+            return {value: record, done:false}
+          }
+          return {value: undefined, done: true}
+        },
+      };
+    },
+  };
   const dataset: Map<number, ArqRecord> = new Map();
-  const collection = path.basename(outFile);
+  const collection = path.basename(inFile);
   // let recordSize = 0;
   parser.on('readable', () => {
-    let record;
+
     // tslint:disable-next-line:no-conditional-assignment
-    while (record = parser.read()) {
+    for (const record of iterable) {
       dataset.set(record.id, record);
       // console.log('Read records ' + ++recordSize);
     }
@@ -62,11 +76,13 @@ const processFile = (inFile: string, outFile: string) => {
   });
   inStream.pipe(parser);
   let output = '';
-  return one.then((ds: Map<number, ArqRecord>) => {
+  return one
+    .then((ds: Map<number, ArqRecord>) => {
+    log('processing %o elements from %o', ds.size, collection);
     for (const entry of ds.values()) {
       output += extract(entry.formula, String(entry.id), entry.post_id);
     }
-    vlog("Plan to write entries from %o", inFile);
+    vlog('Plan to write entries from %o', inFile);
     return writeFile(outFile, output);
   });
 
@@ -77,16 +93,16 @@ export const Arq2Hrvst = (inFile: string, outFile: string): any => {
   const header = writeFile(
     consolidatedOut,
     '<mws:harvest xmlns:mws="http://search.mathweb.org/ns" data-set="mse" data-doc-id="consolidated" data-collection="00">\n',
-    { encoding: 'utf8'}
+    { encoding: 'utf8' },
   );
   let count = 0;
   // @ts-ignore
   return header.then(() => afs.readdirAsync(inFile))
-    .map((file:string) => {
+    .map((file: string) => {
       count++;
       log('schedule file %o total %d', file, count);
       return processFile(path.join(inFile, file as string), consolidatedOut);
-    }, { concurrency: 20  })
+    }, { concurrency: 20 })
     .then(() => (writeFile(consolidatedOut, '</mws:harvest>\n')))
     .then(() => count);
 };
