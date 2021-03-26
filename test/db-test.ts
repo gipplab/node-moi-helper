90eda41472c9d140d79a96a2b6d18b0778813285
physikerwelt (Moritz Schubotz) <wiki@physikerwelt.de>
2021-03-26 18:36:08 +0100

    Only run db tests if env variable is set

--- test/db-test.ts
+++ test/db-test.ts
@@ -2,11 +2,11 @@ import { expect } from 'chai';
 import 'mocha';
 import * as pgPromise from 'pg-promise';
 
-
 describe('DB', () => {
-
-  it('now',
-    () => (pgPromise())({})
-      .one('SELECT NOW()')
-      .then((res) => expect(res).property('now')));
+  ((process.env.PGDATABASE) ? it : it.skip)('now',
+    () => {
+      return (pgPromise())({})
+        .one('SELECT NOW()')
+        .then((res) => expect(res).property('now'));
+    });
 });
